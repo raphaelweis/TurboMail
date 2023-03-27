7cfbe3f19efd1cb7027f60dce63a2d2018defe2c
Raphaël Weis <raphael.weis.2003@gmail.com>
2023-03-27 22:07:56 +0200

    added javascript function to receive input from signup form

--- src/javascript/form.js
+++ src/javascript/form.js
@@ -1,13 +1,40 @@
 //handling of signIn form
 function signIn() {
-  var signInForm = document.getElementById("sign-in");
+  let signInForm = document.getElementById("sign-in");
 
   signInForm.addEventListener("submit", function (event) {
     event.preventDefault(); //prevent the html from auto submitting data
 
-    var email = document.getElementById("si-email").value;
+    let email = document.getElementById("si-email").value;
+    let password = document.getElementById("si-password").value;
+
     console.log(email);
-    var password = document.getElementById("si-password").value;
     console.log(password);
   });
 }
+
+//handling of signUp form
+function signUp() {
+  let signUpForm = document.getElementById("sign-up");
+
+  signUpForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    let firstname = document.getElementById("su-firstname").value;
+    let lastname = document.getElementById("su-lastname").value;
+    let email = document.getElementById("su-email").value;
+    let password1 = document.getElementById("password1").value;
+    let password2 = document.getElementById("password2").value;
+
+    console.log(firstname);
+    console.log(lastname);
+    console.log(email);
+    console.log(password1);
+    console.log(password2);
+  });
+}
+
+window.onload = function () {
+  signIn();
+  signUp();
+};

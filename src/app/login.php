6cbe94de0d293b27cf17bb5c974204b22e1aa42b
Raphaël Weis <raphael.weis.2003@gmail.com>
2023-05-08 20:16:47 +0200

    deleted old formatter files, added a package.json file, restarted writing login.php

--- src/app/login.php
+++ src/app/login.php
@@ -1,27 +1,5 @@
 <?php
 
-// require_once __DIR__ . "/classes/User.php";
-// require_once "/home/raphaelw/Projects/TurboMail/src/app/classes/User.php";
-require_once __DIR__ . '/classes/User.php';
-
-new User("bonjour", "password");
-
-// echo "success";
-echo 910;
-
-// $errors = array();
-// if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
-//     $errors[] = 500;
-//     exit($errors);
-// }
-// if (!isset($_POST["type"])) {
-//     $errors[] = 500;
-//     exit($errors);
-// }
-// if ($_POST["type"] == "login") {
-//     if (isset($_POST["email"]) && isset($_POST["password"])) {
-//         $email = $_POST["email"];
-//         $password = $_POST["password"];
-//         $user = new User($email, $password);
-//     }
-// }
+if (empty($_POST)) {
+    exit(1);
+}
--- src/public/login/js/router.js
+++ src/public/login/js/router.js
@@ -1,83 +1,38 @@
 // classes imports
-import * as ValidationErrors from "../../../lib/js/validationErrors.js";
 
 // defining path constants
 const LOGIN_URL = "../../app/login.php";
 
 // add event Listeners on page load - prevent default submit
 window.onload = function () {
-  const signInForm = document.getElementById("sign-in");
-  signInForm.addEventListener("submit", (event) => {
-    event.preventDefault();
-    formRequest(signInForm, "login");
-  });
-  const signUpForm = document.getElementById("sign-up");
-  signUpForm.addEventListener("submit", (event) => {
-    event.preventDefault();
-    formRequest(signUpForm, "register");
-  });
+    const signInForm = $("#sign-in");
+    signInForm.submit(function (event) {
+        event.preventDefault();
+        signInRequest();
+    });
+    const signUpForm = $("#sign-up");
+    signUpForm.submit(function (event) {
+        event.preventDefault();
+        signUpRequest();
+    });
 };
 
-function formRequest(form, type) {
-  const formCopy = form;
-  const formData = new FormData(formCopy);
-  formData.append("type", type);
+function signInRequest() {
+    // remove previous error divs
+    const validationErrorsDivs = $("validation-errors")
+    validationErrorsDivs.forEach((div) => div.remove());
 
-  // remove previous error divs
-  const validationErrorsDivs = document.querySelectorAll(".validation-error");
-  validationErrorsDivs.forEach((div) => div.remove());
-
-  //use of the fetch API to send a POST request
-  fetch(LOGIN_URL, {
-    method: "POST",
-    body: formData,
-  })
-    .then((response) => response.text())
-    .then((responseText) => {
-      if (responseText === "success") {
-        window.location.href = "../home/home.html";
-      }
-      let errorCode = parseInt(responseText);
-      checkErrorCodes(errorCode, formCopy);
-    })
-    .catch((error) => {
-      console.log(error);
-      if (error instanceof ValidationErrors.ValidationError) {
-        error.appendError();
-      } else {
-        alert("Oops, an unexpected server error happened !");
-      }
-    });
+    $.post(LOGIN_URL, $("#sign-in").serialize(), function (response) {
+        if (response === 0) {
+            window.location.href = "../../home/home.html";
+        } else if (response === 1) {
+            alert("Login failed !");
+        } else {
+            alert("Oops, something unexpected happened...");
+        }
+    }, "text");
 }
 
-function checkErrorCodes(errorCode, form) {
-  const INVALID_EMAIL = 910;
-  const INVALID_PASSWORD = 911;
-  const INVALID_FIRST_NAME = 912;
-  const INVALID_LAST_NAME = 913;
-  const EMAIL_TAKEN = 914;
-  const NON_MATCHING_PASSWORDS = 915;
-  const EMAIL_NOT_FOUND = 916;
-  const WRONG_PASSWORD = 917;
+function signUpRequest(form) {
 
-  switch (errorCode) {
-    case INVALID_EMAIL:
-      throw new ValidationErrors.InvalidEmail(form.elements["email"]);
-    case INVALID_PASSWORD:
-      throw new ValidationErrors.InvalidPassword(form.elements["password"]);
-    case INVALID_FIRST_NAME:
-      throw new ValidationErrors.InvalidFirstName(form.elements["firstname"]);
-    case INVALID_LAST_NAME:
-      throw new ValidationErrors.InvalidLastName(form.elements["lastname"]);
-    case EMAIL_TAKEN:
-      throw new ValidationErrors.EmailTaken(form.elements["email"]);
-    case NON_MATCHING_PASSWORDS:
-      throw new ValidationErrors.NonMatchingPasswords(
-        form.elements["password-check"]
-      );
-    case EMAIL_NOT_FOUND:
-      throw new ValidationErrors.EmailNotFound(form.elements["email"]);
-    case WRONG_PASSWORD:
-      throw new ValidationErrors.WrongPassword(form.elements["password"]);
-  }
 }

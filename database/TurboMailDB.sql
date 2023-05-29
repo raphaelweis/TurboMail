5449abe3610f0d73bb7252a026a85df34b49b5c0
sambarthazon <sam.barthazon@gmail.com>
2023-05-29 15:16:32 +0200

    changed database file

--- database/TurboMailDB.sql
+++ database/TurboMailDB.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: May 22, 2023 at 07:12 AM
+-- Generation Time: May 29, 2023 at 03:15 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -24,37 +24,110 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `users`
+-- Table structure for table `Message`
 --
 
-CREATE TABLE `users` (
-  `ID` bigint(20) NOT NULL,
-  `FirstName` varchar(100) NOT NULL,
-  `LastName` varchar(100) NOT NULL,
-  `Email` varchar(100) NOT NULL,
-  `Password` varchar(127) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+CREATE TABLE `Message` (
+  `id` bigint(20) NOT NULL,
+  `id_sender` bigint(20) NOT NULL,
+  `id_receiver` bigint(20) NOT NULL,
+  `message` text NOT NULL,
+  `date` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Relation`
+--
+
+CREATE TABLE `Relation` (
+  `id` bigint(20) NOT NULL,
+  `id_sender` bigint(20) NOT NULL,
+  `id_receiver` bigint(20) NOT NULL,
+  `status` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `User`
+--
+
+CREATE TABLE `User` (
+  `id` bigint(20) NOT NULL,
+  `first_name` varchar(128) NOT NULL,
+  `last_name` varchar(128) NOT NULL,
+  `email` varchar(256) NOT NULL,
+  `password` varchar(256) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `users`
+-- Indexes for table `Message`
 --
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`ID`),
-  ADD UNIQUE KEY `Email` (`Email`);
+ALTER TABLE `Message`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_sender` (`id_sender`),
+  ADD KEY `id_receiver` (`id_receiver`);
+
+--
+-- Indexes for table `Relation`
+--
+ALTER TABLE `Relation`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_sender` (`id_sender`),
+  ADD KEY `id_receiver` (`id_receiver`);
+
+--
+-- Indexes for table `User`
+--
+ALTER TABLE `User`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `UNIQUE` (`email`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT for table `users`
+-- AUTO_INCREMENT for table `Message`
 --
-ALTER TABLE `users`
-  MODIFY `ID` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+ALTER TABLE `Message`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `Relation`
+--
+ALTER TABLE `Relation`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `User`
+--
+ALTER TABLE `User`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `Message`
+--
+ALTER TABLE `Message`
+  ADD CONSTRAINT `Message_ibfk_1` FOREIGN KEY (`id_sender`) REFERENCES `User` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `Message_ibfk_2` FOREIGN KEY (`id_receiver`) REFERENCES `User` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `Relation`
+--
+ALTER TABLE `Relation`
+  ADD CONSTRAINT `Relation_ibfk_1` FOREIGN KEY (`id_sender`) REFERENCES `User` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `Relation_ibfk_2` FOREIGN KEY (`id_receiver`) REFERENCES `User` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;

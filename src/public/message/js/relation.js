ec9fe8282117e5dada33a4957e8b74e18d6aebf2
sambarthazon <sam.barthazon@gmail.com>
2023-05-30 16:47:30 +0200

    added form to send a relation

--- src/public/message/js/relation.js
+++ src/public/message/js/relation.js
@@ -1,8 +1,9 @@
 export function resizeTextArea() {
     $("#asking-message").each(function () {
-        this.setAttribute("style", "height:" + (this.scrollHeight) + "px;overflow-y:hidden;");
+        this.setAttribute("style", "height:" + (this.scrollHeight) + "px;overflow-y:scroll;");
     }).on("input", function () {
         this.style.height = 0;
-        this.style.height = (this.scrollHeight) + "px";
+        this.style.height = this.scrollHeight + "px";
+        this.style.maxHeight = 25 + "em";
     });
 }

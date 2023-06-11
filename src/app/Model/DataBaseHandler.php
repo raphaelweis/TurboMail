a14914300d174da82b3075d7f8b15644a8c4cad1
Raphaël Weis <raphael.weis.2003@gmail.com>
2023-06-11 10:43:23 +0200

    added basic documentation, corrected little imprecision in login.html

--- src/app/Model/DataBaseHandler.php
+++ src/app/Model/DataBaseHandler.php
@@ -7,7 +7,14 @@ include_once __DIR__ . '/../const/global.php';
 use PDO;
 use PDOException;
 
+/**
+ * The DataBaseHandler class provides an abstraction to identify the objects that can interact with the database.
+ */
 class DataBaseHandler {
+    /**
+     * this method connects its caller to the configured database
+     * @return PDO|void
+     */
     public function connect() {
         try {
             return new PDO(
